7014f792bf8f46c923427a00cd8f9c78cf4dd2b8
Diego Alvarez <andresdaa2015@gmail.com>
2021-04-27 00:47:59 -0400

    Erase Exercises

--- exercises/chapter_two/exercise1.ex
+++
@@ -1,9 +0,0 @@
-defmodule FirstProblem do
-  @slice_bread 0.10
-  @bottle_milk 2
-  @cake 15
-  def total_spent(slices, bottles, cakes) do
-    total = (@slice_bread * slices) + (@bottle_milk * bottles) + (@cake * cakes)
-    IO.puts(total)
-  end
-end
